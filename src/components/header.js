10bb30959faf1a71aac03702e6eb7333bd69249f
Konstantin Mishur <kmishur@its.jnj.com>
2020-10-06 16:57:02 +0300

    Header menu: Highlight active state

--- src/components/header.js
+++ src/components/header.js
@@ -4,15 +4,7 @@ import Menu from './menu'
 
 const Header = ({ title, description }) => (
   <div className="header container">
-    <div className="flex-always">
-      <Menu />
-      <div className="flex">
-        <Link activeClassName="active" partiallyActive={true} className="btn get-test" to="/quiz/">Пройти тест</Link>
-        <span className="get-test-des hidden md:visible text-sm">
-          Оцените риск <br />заболевания за 5 мин
-        </span>
-      </div>
-    </div>
+    <Menu />
     {/* <h1>
       { title }
     </h1>
--- src/components/menu.js
+++ src/components/menu.js
@@ -3,21 +3,28 @@ import { Link } from "gatsby"
 
 const Menu = () => {
   const [ active, setActive ] = useState('');
-
-  const toggleActive = () => active === 'active' ? setActive('') : setActive('active');
+  const toggleActive = () => active === 'active' ? setActive('') : setActive('expanded');
 
   return (
-    <div className={`menu ${active}`}>
-      <a className="btn menu-toggle sm:hidden" onClick={toggleActive}>
-        <span className="hamburger" />
-        <span className="hamburger" />
-        <span className="hamburger" />
-      </a>
-      <div className="menu-links hidden sm:visible">
-        <Link activeClassName="active" className="col" to="/">Главная</Link>
-        <Link activeClassName="active" partiallyActive={true} className="col" to="/about-project">О проекте</Link>
-        <Link activeClassName="active" partiallyActive={true} className="col" to="/diagnose">Получить помощь</Link>
-        <Link activeClassName="active" partiallyActive={true} className="col" to="/about-cancer">О раке груди</Link>
+    <div className="flex-always">
+      <div className={`menu ${active}`}>
+        <a className="btn menu-toggle sm:hidden" onClick={toggleActive}>
+          <span className="hamburger" />
+          <span className="hamburger" />
+          <span className="hamburger" />
+        </a>
+        <div className="menu-links hidden sm:visible">
+          <Link activeClassName="active" className="" to="/">Главная</Link>
+          <Link activeClassName="active" partiallyActive={true} className="" to="/about-project">О проекте</Link>
+          <Link activeClassName="active" partiallyActive={true} className="" to="/diagnose">Получить помощь</Link>
+          <Link activeClassName="active" partiallyActive={true} className="" to="/about-cancer">О раке груди</Link>
+        </div>
+      </div>
+      <div className="flex">
+        <Link activeClassName="active" partiallyActive={true} className="btn get-test" to="/quiz/">Пройти тест</Link>
+        <span className="get-test-des hidden md:visible text-sm">
+          Оцените риск <br />заболевания за 5 мин
+        </span>
       </div>
     </div>
   );
